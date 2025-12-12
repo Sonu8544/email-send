7fdc07ea6cba35c9f337daa6ae66f0a8f5a07703
Sonu8544 <sksonu8544@gmail.com>
2025-12-12 22:53:38 +0530

    fix port

--- server/index.js
+++ server/index.js
@@ -327,6 +327,7 @@ app.post("/contact", (req, res, next) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
